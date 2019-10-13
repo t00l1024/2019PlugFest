883d0283c40bb4afc48a5417fee9f7b2385a6ded
user42 <timothy.toole@gmail.com>
2019-10-13 12:26:23 -0600

    Update make_hosts.sh scrip to reflect 3 namespaces

--- make_hosts.sh
+++ make_hosts.sh
@@ -3,30 +3,19 @@
 # Source bashrc-of
 source bashrc-of
 
-# Create Test Namespaces and Connect them - atx950
-create_ns wan 192.88.99.254/24
-as_ns wan ip route add default via 192.88.99.1
-sudo ovs-vsctl add-port atx950 veth-wan \
-		        -- set interface veth-wan ofport_request=4
+# Create Test Namespaces and Connect them 
+create_ns host1 192.168.10.2/24
+as_ns host1 ip route add default via 192.168.10.1
+sudo ovs-vsctl add-port t2-sw1 veth-host1 \
+		        -- set interface veth-host1 ofport_request=3
 
-create_ns dns 192.168.11.2/24
-as_ns dns ip route add default via 192.168.11.1
-sudo ovs-vsctl add-port atx950 veth-dns \
-		        -- set interface veth-dns ofport_request=5
+create_ns host2 192.168.20.2/24
+as_ns host2 ip route add default via 192.168.20.1
+sudo ovs-vsctl add-port t2-sw2 veth-host2 \
+		        -- set interface veth-host2 ofport_request=3
 
-create_ns host2 192.168.11.3/24
-as_ns host2 ip route add default via 192.168.11.1
-sudo ovs-vsctl add-port atx950 veth-host2 \
-		        -- set interface veth-host2 ofport_request=6
-
-# Create Test Namespaces and Connect them - ar2930f-1
-create_ns host3 192.168.11.4/24
-as_ns host3 ip route add default via 192.168.11.1
-sudo ovs-vsctl add-port ar2930f-1 veth-host3 \
-		        -- set interface veth-host3 ofport_request=5
-
-# Create Test Namespaces and Connect them - ar2930f-1
-create_ns host4 192.168.51.2/24
-as_ns host4 ip route add default via 192.168.51.1
-sudo ovs-vsctl add-port ar2930f-1 veth-host4 \
-		        -- set interface veth-host4 ofport_request=50
+# Create Poseidon namespace (for testing)
+create_ns poseidon 192.168.0.2/24
+as_ns poseidon ip route add default via 192.168.0.1
+sudo ovs-vsctl add-port t1-sw2 veth-poseidon \
+		        -- set interface veth-poseidon ofport_request=3
