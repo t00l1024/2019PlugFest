68dfbaf2251e1c58fac508f995b00c641c78c0bd
user42 <timothy.toole@gmail.com>
2019-10-13 18:37:33 -0600

    Fixed LACP links to ext-net

--- make_network.sh
+++ make_network.sh
@@ -62,22 +62,41 @@ ovs_patch t1-sw2 2 t2-sw1 2
 # Create patch between t1-sw2 port 1, to t2-sw2 port 1
 ovs_patch t1-sw2 1 t2-sw2 1
 
-# Create MLAGs to External Network
-#ovs_patch t1-sw1 4 ext-net 1
-#ovs_patch t1-sw1 5 ext-net 2
-#ovs_patch t1-sw2 4 ext-net 3
-#ovs_patch t1-sw2 5 ext-net 4
-
-# This fails!!!!
 
 # Set ports as bond ports on External Network
-#sudo ovs-vsctl add-bond ext-net bond0 ext-net-1 ext-net-2 \
-#	-- set port bond0 lacp=active \
-#	-- set port bond0 bond_mode=balance-tcp
+sudo ovs-vsctl add-bond ext-net bond0 ext-net-1 ext-net-2 lacp=active \
+        -- set interface ext-net-1 type=patch \
+        -- set interface ext-net-1 options:peer=t1-sw1-4 \
+        -- set interface ext-net-2 type=patch \
+        -- set interface ext-net-2 options:peer=t1-sw1-5
 
-#sudo ovs-vsctl add-bond ext-net bond1 ext-net-3 ext-net-4 \
-#	-- set port bond1 lacp=active \
-#	-- set port bond1 bond_mode=balance-tcp
+sudo ovs-vsctl add-bond ext-net bond1 ext-net-3 ext-net-4 lacp=active \
+        -- set interface ext-net-3 type=patch \
+        -- set interface ext-net-3 options:peer=t1-sw2-4 \
+        -- set interface ext-net-4 type=patch \
+        -- set interface ext-net-4 options:peer=t1-sw2-5
+
+# Create the other 1/2 of the patch ports. FAUCET will handle LACP
+sudo ovs-vsctl add-port t1-sw1 t1-sw1-4 \
+        -- set interface t1-sw1-4 type=patch \
+        -- set interface t1-sw1-4 options:peer=ext-net-1 \
+        -- set interface t1-sw1-4 ofport_request=4
+
+sudo ovs-vsctl add-port t1-sw1 t1-sw1-5 \
+        -- set interface t1-sw1-5 type=patch \
+        -- set interface t1-sw1-5 options:peer=ext-net-2 \
+        -- set interface t1-sw1-5 ofport_request=5
+
+sudo ovs-vsctl add-port t1-sw2 t1-sw2-4 \
+        -- set interface t1-sw2-4 type=patch \
+        -- set interface t1-sw2-4 options:peer=ext-net-3 \
+        -- set interface t1-sw2-4 ofport_request=4
+
+sudo ovs-vsctl add-port t1-sw2 t1-sw2-5 \
+        -- set interface t1-sw2-5 type=patch \
+        -- set interface t1-sw2-5 options:peer=ext-net-4 \
+        -- set interface t1-sw2-5 ofport_request=5
+
 
 
 # git pull origin master
